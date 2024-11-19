ca7c3a26fe7eab899243ce34693335544cdf826b
markusmikk100 <TauriTammela272@gmail.com>
2024-11-19 16:26:18 +0200

    FUCK ALL

---
+++ src/main/java/net/markusmikk/tutorialmod/item/Moditems.java
@@ -0,0 +1,13 @@
+package net.markusmikk.tutorialmod.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItem;
+import net.markusmikk.tutorialmod.entity.ModEntities;
+import net.minecraft.item.Item;
+import net.minecraft.item.SpawnEggItem;
+
+public class Moditems {
+
+   // FOR GOLEM SPAWN EGG >> ALSO NEED TO ADD 2 DATAGEN 28:26
+  //  public static final Item GOLEM_SPAWN_EGG = registerItem("golem_spawn-egg"),
+  //          new SpawnEggItem(ModEntities.GOLEM, 0x66666, 0x3b200f, new FabricItem.Settings()));
+}
